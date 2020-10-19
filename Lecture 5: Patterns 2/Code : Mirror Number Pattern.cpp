63086a3208581293dafa6ab5bb96f65bfc58c299
lavishabhambri <56781728+lavishabhambri@users.noreply.github.com>
2020-10-19 16:23:20 +0530

    Create Code : Mirror Number Pattern.cpp

---
+++ Lecture 5: Patterns 2/Code : Mirror Number Pattern.cpp
@@ -0,0 +1,64 @@
+/*
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+
+
+
+
+The dots represent spaces.
+
+
+
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 50
+Sample Input 1:
+3
+Sample Output 1:
+      1 
+    12
+  123
+Sample Input 2:
+4
+Sample Output 2:
+      1 
+    12
+  123
+1234
+*/
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        //spaces
+        int space=1;
+        while(space<=n-i){
+            cout<<" ";
+            space++;
+        }
+ 
+        //numbers
+        int j=1;
+        while(j<=i){
+            cout<<j;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    } 
+}
+
+
