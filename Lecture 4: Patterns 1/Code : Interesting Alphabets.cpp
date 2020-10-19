e004669784291403a1a02da46000d80f14552145
lavishabhambri <56781728+lavishabhambri@users.noreply.github.com>
2020-10-19 16:16:30 +0530

    Create Code : Interesting Alphabets.cpp

---
+++ Lecture 4: Patterns 1/Code : Interesting Alphabets.cpp
@@ -0,0 +1,62 @@
+/*
+Print the following pattern for the given number of rows.
+Pattern for N = 5
+E
+DE
+CDE
+BCDE
+ABCDE
+Input format :
+N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 26
+Sample Input 1:
+8
+Sample Output 1:
+H
+GH
+FGH
+EFGH
+DEFGH
+CDEFGH
+BCDEFGH
+ABCDEFGH
+Sample Input 2:
+7
+Sample Output 2:
+G
+FG
+EFG
+DEFG
+CDEFG
+BCDEFG
+ABCDEFG
+*/
+
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    
+    /*  Read input as specified in the question.
+     * Print output as specified in the question.
+     */
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        char startChar ='A' + n-i;
+        int j=1;
+        while (j<=i){
+            char ch= startChar + j-1;
+            cout<<ch;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    
+}
