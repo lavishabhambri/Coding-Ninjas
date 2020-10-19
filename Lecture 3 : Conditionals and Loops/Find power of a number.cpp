fe3c4fd31813001a26c887eb66a129c169893ea3
lavishabhambri <56781728+lavishabhambri@users.noreply.github.com>
2020-10-19 16:03:13 +0530

    Create Find power of a number.cpp

---
+++ Lecture 3 : Conditionals and Loops/Find power of a number.cpp
@@ -0,0 +1,37 @@
+/*
+Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to print the answer.
+Note : For this question, you can assume that 0 raised to the power of 0 is 1
+
+
+Input format :
+Two integers x and n (separated by space)
+Output Format :
+x^n (i.e. x raise to the power n)
+Constraints:
+0 <= x <= 8 
+0 <= n <= 9
+Sample Input 1 :
+ 3 4
+Sample Output 1 :
+81
+Sample Input 2 :
+ 2 5
+Sample Output 2 :
+32
+*/
+
+#include<iostream>
+using namespace std;
+int main() {
+	// Write your code here
+    int x,n;
+    cin>>x>>n;
+    int i=1;
+    int c=1;
+    while(i<=n){
+        c = c*x;
+        i +=1;
+    }
+    cout<<c;
+	
+}
